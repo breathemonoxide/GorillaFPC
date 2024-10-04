27a7d96ca3c4d4cc29459d55c93929286c44d43e
monoxide <numpad0000@gmail.com>
2024-10-04 20:27:34 +0100

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace GorillaFPC
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.breathemonoxide.gorillatag.gorillafpc";
+        public const string Name = "GorillaFPC";
+        public const string Version = "1.0.0";
+    }
+}
